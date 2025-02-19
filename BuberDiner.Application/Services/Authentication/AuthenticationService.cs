d4c6baa0d9d3558712e2fc2b7663a67d3831d47c
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2025-02-19 23:49:55 +0300

    Generate JwtTokens

--- BuberDiner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDiner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,7 +1,17 @@
+using BuberDiner.Application.Common.Interfaces.Authentication;
+
 namespace BuberDiner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
     public AuthenticationResult Login(string email, string password)
     {
         return new AuthenticationResult(
@@ -16,12 +26,22 @@ public class AuthenticationService : IAuthenticationService
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
+        // Check if user already exists
+        
+        
+        // Create user (generate unique Id)
+        
+        // Create JWT Token
+        var userId = Guid.NewGuid();
+        
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+        
         return new AuthenticationResult(
-            Guid.NewGuid(), 
+            userId, 
             firstName, 
             lastName, 
             email, 
-            "token"
+            token
             );
     }
 }
